a23ddf16a601e5fd02b8729c3d2e8a7d7ab68a0b
azis1g1 <76503784+azis1g1@users.noreply.github.com>
2020-12-23 22:12:41 +0800

    Add files via upload

---
+++ Lib/info.js
@@ -0,0 +1,43 @@
+exports.info = (id, Azis, corohelp, tampilTanggal, tampilWaktu, instagram, nomer, aktif, groupwhatsapp, youtube) => {
+	return `
+  
+❉──────────❉
+  Hi. *${id.split("@s.whatsapp.net")[0]}* 👋️
+❉──────────❉
+
+         ───
+📆 *${tampilTanggal}*
+⏱️ *${tampilWaktu}*
+📢 Bot Aktif ; *${aktif}*
+         ───
+         
+╔════════════════════
+║ *About ${Azis}*
+╠════════════════════
+║   *🍭HALLO KAKAK🍭*
+╠════════════════════
+╠════════════════════
+║ *OWNER BY : Azis
+║  *CREATOR BY : NEBULA CEWE GW
+╠════════════════════
+║├≽️⚜ *SEMANGAT*: _Chichi ID
+║├≽️⚜ *DESIGNER*: _BangLent ID_
+║├≽️⚜ *KONTAK GW*: _-081649175378-,
+╠════════════════════
+╠════════════════════
+║  *${A187}*
+╠════════════════════
+║╭──❉ *SOSMED ADMIN* ❉──
+║│1. *Group WhatsApp*
+║│ _${groupwhatsapp}_
+║│2. *YouTube <subscribe>*
+║│ _${youtube}_
+║│3. *Instagram <Follow>*
+║│ _${instagram}_
+║│4. *Creator ${A187}*
+║│ _${nomer}_
+║╰───────────
+╠════════════════════
+║ _*SALAM DARI PEMBUAT NYA*_
+╚════════════════════`
+}
